55565c0f2e30ca34225bc8d210ef20f716ba5b21
Luís Guilherme <120354142+gui2077@users.noreply.github.com>
2023-07-03 19:58:39 -0300

    Listas da forma mais indicada
    Fiz uma modificação na forma como listas são feitas, buscando pelo ID, como normalmente é cobrado em bancos de dados

--- 03_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
+++ 03_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
@@ -5,6 +5,20 @@ const ListRender = () => {
     const [lista] = useState(["Matheus", "Pedro", "Josias", "Maria"])
     // Quando tem um [] significa que aí será uma lista
 
+    const [users, setUsers] = useState([
+      {id: 1, name: "Luís Guilherme", age: 23},
+      {id: 2, name: "Cláudio Ferraz", age: 44},
+      {id: 3, name: "Márcio Serpa", age: 39}
+    ])
+
+    const deleteRandom = () => {
+      const randomNumber = Math.floor(Math.random() * 4)
+
+      setUsers((prevUsers) => {
+        return prevUsers.filter((user) => randomNumber !== user.id)
+      })
+    }
+
   return (
     <div>
         <ul>
@@ -13,6 +27,17 @@ const ListRender = () => {
                 // Aqui há uma captação dos elementos da lista gerada como variável
             ))}
         </ul>
+
+        {/* O mais indicado, porém, é fazer de outra forma, em que há IDs, assim: */}
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.age}
+            </li>
+          ))}
+        </ul>
+        {/* Aqui, vale a pena lembrar de Banco de Dados, em que o atributo chave (chave única), nesse caso, será o ID. Mas aqui estamos exibindo apenas o nome e a idade, apenas. */}
+        <button onClick={deleteRandom}>Delete random user</button>
     </div>
   )
 }
